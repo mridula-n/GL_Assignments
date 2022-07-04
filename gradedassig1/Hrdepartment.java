abdd1800484c07ee513508386c4d638193763672
mridula-n <72420855+mridula-n@users.noreply.github.com>
2022-07-04 23:32:29 +0530

    Add files via upload

---
+++ gradedassig1/Hrdepartment.java
@@ -0,0 +1,25 @@
+package com.greatlearning.javaprog.gradedassig1;
+
+public class Hrdepartment extends SuperDepartment {
+
+	String departmentName() {
+		String departmentName = "HR Department";
+		return departmentName;
+	}
+
+	String getTodaysWork() {
+		String getTodaysWork = "Fill today’s timesheet and mark your attendance";
+		return getTodaysWork;
+	}
+
+	String getWorkDeadline() {
+		String getWorkDeadline = "Complete by EOD";
+		return getWorkDeadline;
+	}
+
+	String doActivity() {
+		String doActivity = "team Lunch";
+		return doActivity;
+	}
+
+}
---
+++ gradedassig1/SuperDepartment.java
@@ -0,0 +1,25 @@
+package com.greatlearning.javaprog.gradedassig1;
+
+public class SuperDepartment {
+
+	String departmentName() {
+		String departmentName = "Super Department";
+		return departmentName;
+	}
+
+	String getTodaysWork() {
+		String getTodaysWork = "No Work as of now";
+		return getTodaysWork;
+	}
+
+	String getWorkDeadline() {
+		String getWorkDeadline = "Nil";
+		return getWorkDeadline;
+	}
+
+	String isTodayAHoliday() {
+		String isTodayAHoliday = "Today is not a holiday";
+		return isTodayAHoliday;
+	}
+
+}
